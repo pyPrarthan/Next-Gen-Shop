e07b637cd1c6a68637ea1015ca76d49b17add352
pyPrarthan <christianprarthan07@gmail.com>
2025-01-07 11:43:42 -0500

    server.js updated

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import productRoute from './routes/productRoute.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res)=>{
     res.send('Server is ready');
@@ -17,7 +18,7 @@ app.use(express.json()); //middleware to parse json data
 app.use('/api/products', productRoute);
 
 
-app.listen(5000, ()=>{
+app.listen(PORT, ()=>{
     connectDB();
-    console.log('Server is running on port http://localhost:5000' );
+    console.log('Server is running on port http://localhost:' + PORT);
 }) 
