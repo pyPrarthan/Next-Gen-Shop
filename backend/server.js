a112e801e6b1c5aa66aec9a87087a8fd88ad6a3f
pyPrarthan <christianprarthan07@gmail.com>
2025-01-09 22:54:08 -0500

    updated code

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import path from 'path';
 import { connectDB } from './config/db.js';
 import productRoute from './routes/productRoute.js';
 
@@ -9,6 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.get('/', (req, res)=>{
     res.send('Server is ready');
 })
@@ -16,6 +18,14 @@ app.get('/', (req, res)=>{
 app.use(express.json()); //middleware to parse json data
 
 app.use('/api/products', productRoute);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+    app.get('*', (req, res)=>{
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    })
+}
 
 
 app.listen(PORT, ()=>{
